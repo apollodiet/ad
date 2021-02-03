4da06c6e34b25b6f1e937112241e416adba7fac4
Apollo Diet Admin <info@apollodiet.com>
2021-02-04 02:27:33 +0530

    commit images and js files

---
+++ js/emailValidation.js
@@ -0,0 +1,10 @@
+﻿function validateEmail(sEmail) {
+    var filter = /^[\w-.+]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+
+    if (filter.test(sEmail)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
